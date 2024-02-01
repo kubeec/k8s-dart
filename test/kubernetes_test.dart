97c9c653b3f1710f3f284989660d86a90a350ca5
Fatih Sever <fatih@fatihsever.com>
2024-02-01 14:27:33 +0300

    Add kubernetes_test.dart

---
+++ test/kubernetes_test.dart
@@ -0,0 +1,15 @@
+// import 'package:k8s/k8s.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Kubeconfig', () {
+    // final kubernetes = Kubernetes();
+
+    test('client can be instantiated with initDefault method', () async {
+      // TODO(fatih): Mock
+      // await kubernetes.initDefault();
+      // expect(kubernetes.client, isNotNull);
+      expect(true, isTrue);
+    });
+  });
+}
